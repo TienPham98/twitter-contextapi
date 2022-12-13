004d728e3dfbaab30ea8305f1d6742a4e7ace0e4
Pham Nhat Tien <104922737+TienPham98@users.noreply.github.com>
2022-12-13 16:26:14 +0700

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -41,7 +41,7 @@ export const authOptions = {
     },
     async signIn({ account, profile }) {
       if (account.provider === "google") {
-        return profile.email_verified && profile.email.endsWith("@example.com")
+        return profile.email_verified && profile.email.endsWith("@gmail.com")
       }
       return true // Do different verification for other providers that don't have `email_verified`
     },
